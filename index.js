00a396384d663d9f050a90e017d7248d8ff7d8ba
xena <74474948+a-andres1@users.noreply.github.com>
2021-03-10 22:55:11 -0500

    added requires to the inquirer and set up prompts

---
+++ index.js
@@ -0,0 +1,13 @@
+// adds inquirer package
+const inquirer = require('inquirer');
+// adds fs package
+const fs = require('fs');
+
+
+// begin inquirer prompts
+inquirer
+    .prompt([
+        {
+            
+        }
+    ])
