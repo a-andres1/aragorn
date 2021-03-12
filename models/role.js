48af21107370f09f144b71b038b9de0d8e02ff31
xena <74474948+a-andres1@users.noreply.github.com>
2021-03-12 18:21:15 -0500

    added class info for tables and sequelize

--- models/role.js
+++ models/role.js
@@ -2,3 +2,36 @@
 const{Model, DataTypes, INTEGER } = require('sequelize');
 // gets the connection to the database
 const sequelize = require('../config/connection')
+
+
+class Role extends Model {}
+
+Role.init(
+    {
+        role_id: {
+            type: DataTypes.INTEGER, 
+            allowNull: false,
+            primaryKey: true,
+        }, 
+        role_title: {
+            type: DataTypes.STRING(30),
+            allowNull: false
+        },
+        role_salary:{
+            type: DataTypes.DECIMAL(6,2)
+        },
+        department_id:{
+            type: DataTypes.INTEGER,
+            allowNull: false
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'role',
+    }
+);
+
+module.exports = Role;
