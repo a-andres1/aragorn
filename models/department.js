94c87a0bcbe95223bf3531c7b0ee4bd1919c35a9
xena <74474948+a-andres1@users.noreply.github.com>
2021-03-12 18:20:24 -0500

    added info for table and sequalize

--- models/department.js
+++ models/department.js
@@ -2,3 +2,28 @@
 const{Model, DataTypes, INTEGER } = require('sequelize');
 // gets the connection to the database
 const sequelize = require('../config/connection')
+
+class Department extends Model {}
+
+Department.init(
+    {
+       department_id: {
+           type: DataTypes.INTEGER, 
+           primaryKey: true,
+           allowNull: false
+       },
+       department_name: {
+           type: DataTypes.STRING(30),
+            allowNull: false
+       }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'department',
+    }
+);
+
+module.exports = Department;
