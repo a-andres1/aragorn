9cd60427c90841c2debb15c750b0d6b24c363eef
xena <74474948+a-andres1@users.noreply.github.com>
2021-04-26 19:16:29 -0400

    creating the employee table

--- db/schema.sql
+++ db/schema.sql
@@ -15,3 +15,12 @@ CREATE TABLE roles (
     PRIMARY KEY (id),
     FOREIGN KEY (department_id) REFERENCES department(id)
 );
+
+CREATE TABLE employee (
+    id INT AUTO_INCREMENT,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (role_id) REFERENCES roles(id)
+);
