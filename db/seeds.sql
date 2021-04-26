83f3d227180d500f5918837fe03f13b4850eb013
xena <74474948+a-andres1@users.noreply.github.com>
2021-04-26 19:18:04 -0400

    creating seed data for tables

---
+++ db/seeds.sql
@@ -0,0 +1,16 @@
+USE employee_db;
+
+INSERT INTO department (dep_name)
+VALUES ("Marketing"),
+("HR"),
+("Sales");
+
+INSERT INTO roles (title, salary, department_id)
+VALUES ("Marketing Specialist", 67000.00, 1),
+("Sales Person", 33000.00, 3),
+("Hiring Manager", 45000.00, 2);
+
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ("Alyssa", "Andres", 1),
+("Andres", "Long", 3),
+("Jeff", "Bezos", 2);
